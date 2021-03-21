dac3132f562d6daec333284ce53f0bd69fb03267
FarisAlNatsheh <80884713+FarisAlNatsheh@users.noreply.github.com>
2021-03-22 01:58:01 +0200

    Update Main_Menu.java

--- Main_Menu.java
+++ Main_Menu.java
@@ -1,24 +1,27 @@
 package platformer;
-import java.awt.Button;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 //Ismail, Faris, Nadeem
 //Date: 21/3/2021
-public class Main_Menu implements ActionListener {
-	JTextArea h,w,x;
+public class Main_Menu implements ActionListener, ItemListener {
+	JTextArea h,w;
+	JLabel x;
 	JButton btn;
 	JCheckBox checkbox1;
 	int width, height;
-
+	boolean checked = false;
+	JFrame frame = new JFrame("Main_Menu");
 	public Main_Menu() {
-		JFrame frame = new JFrame("Main_Menu");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(460, 500);
 		frame.setLocation(430, 100);
@@ -28,20 +31,19 @@ public class Main_Menu implements ActionListener {
 		h = new JTextArea("1080");
 		h.setBounds(160, 150, 50, 30);
 		h.setEditable(true);
-		h.setBackground(Color.LIGHT_GRAY);
-		x = new JTextArea("x");
-		x.setBounds(215, 160, 6, 15);
-		x.setEditable(false);
-		x.setBackground(Color.LIGHT_GRAY);
+		h.setBackground(Color.white);
+		x = new JLabel("x");
+		x.setBounds(215, 160, 15, 15);
 		w = new JTextArea("1920");
 		w.setBounds(227, 150, 50, 30);
 		w.setEditable(true);
-		w.setBackground(Color.LIGHT_GRAY);
+		w.setBackground(Color.white);
 		checkbox1 = new JCheckBox("Fullscreen");    
 		checkbox1.setBounds(180,200, 100,50);
 		btn = new JButton("OK");
 		btn.setBounds(175,250,100,30);
 		btn.addActionListener(this);
+		checkbox1.addItemListener(this);
 		panel.add(checkbox1);
 		panel.add(h);
 		panel.add(x);
@@ -50,14 +52,38 @@ public class Main_Menu implements ActionListener {
 		frame.setVisible(true);
 	}
 	public void actionPerformed (ActionEvent e) {	
-		width = Integer.parseInt(w.getText());
-		height = Integer.parseInt(h.getText());
-		new Main(new Levels().testBench(),20,20,width,height);
+		
+		if(!checked) {
+			width = Integer.parseInt(w.getText());
+			height = Integer.parseInt(h.getText());
+			new Main(new Levels().testBench(),20,20,width,height);
+		}
+		else
+			new Main(new Levels().testBench(),20,20);
+		frame.setVisible(false);
+
 	}
 
 	public static void main(String[] args) {
 		new Main_Menu();
 
+	}
+	
+	public void itemStateChanged(ItemEvent e) {
+		if(e.getStateChange() == 1) {
+			checked = true;
+			h.setEditable(false);
+			h.setBackground(Color.LIGHT_GRAY);
+			w.setEditable(false);
+			w.setBackground(Color.LIGHT_GRAY);
+		}
+		else {
+			checked = false;
+			h.setEditable(true);
+			h.setBackground(Color.white);
+			w.setEditable(true);
+			w.setBackground(Color.white);
+		}
 	}
 
 }
